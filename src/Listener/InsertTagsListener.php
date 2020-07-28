b384fd247fd70311a824aeb5a2c048fd5e665a4e
Arne Borchert <arne.borchert@fipps.de>
2020-07-28 13:50:05 +0200

    Compatbility contao4 9

--- src/Listener/InsertTagsListener.php
+++ src/Listener/InsertTagsListener.php
@@ -1,36 +1,28 @@
 <?php
 /**
  *  Copyright Information
- *  @copyright: 2018 agentur fipps e.K.
- *  @author   : Arne Borchert <arne.borchert@fipps.de>
- *  @license  : LGPL 3.0+
+ *
+ * @copyright: 2018 agentur fipps e.K.
+ * @author   : Arne Borchert <arne.borchert@fipps.de>
+ * @license  : LGPL 3.0+
  */
 
 namespace Fipps\BootstrapCustomizerBundle\Listener;
 
 
-use Contao\CoreBundle\Framework\ContaoFrameworkInterface;
+use Contao\CoreBundle\Framework\Adapter;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\ServiceAnnotation\Hook;
+use Terminal42\ServiceAnnotationBundle\ServiceAnnotationInterface;
 
-class InsertTagsListener
+class InsertTagsListener implements ServiceAnnotationInterface
 {
 
     /**
-     * @var ContaoFrameworkInterface
+     * @Hook("replaceInsertTags")
      */
-    private $framework;
-
-    /**
-     * @var array
-     */
-    private $supportedTags = array();
-
-    /**
-     * Constructor.
-     *
-     * @param ContaoFrameworkInterface $framework
-     */
-    public function __construct(ContaoFrameworkInterface $framework)
+    public function onReplaceInsertTags(string $insertTag, bool $useCache, string $cachedValue, array $flags, array $tags, array $cache, int $_rit, int $_cnt)
     {
-        $this->framework = $framework;
+        return false;
     }
 }
